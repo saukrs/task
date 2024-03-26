ab78584d51d4601c10f049ce8efaaca440c743d6
Saulius Krasuckas <saulius2@ar-fi.lt>
2024-03-26 16:01:05 +0000

    rabbitmq-producer.py: Mark up source of the origin.

--- rabbitmq-producer.py
+++ rabbitmq-producer.py
@@ -1,3 +1,6 @@
+# Via: https://h3xagn.com/streaming-logs-using-rabbitmq/#file-rabbitmq-producer-py
+# Via: https://gist.github.com/h3xagn/8e0f26f8b09937eacc5fe33937f9481a#file-rabbitmq-producer-py
+
 # import libraries
 import time
 import logging
